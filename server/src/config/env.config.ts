44eacb4935417de3072f12cf5c2f820394045043
Debidutta <debiduttaacharya2002@gmail.com>
2025-12-14 13:31:07 +0530

    chore: update package.json to add tree-cli and improve scripts
    fix: correct formatting in env.config.ts and ensure proper export in index.ts
    
    refactor: simplify morgan logger configuration in morgan.config.ts
    
    refactor: streamline globalErrorHandler function in error.middleware.ts
    
    refactor: clean up catchAsync utility function for better readability

--- server/src/config/env.config.ts
+++ server/src/config/env.config.ts
@@ -1,7 +1,7 @@
 export const envConfig = {
-    PORT: process.env.PORT || 8080,
-    NODE_ENV: process.env.NODE_ENV || 'development',
-    DB: {
-        MONGO_URI: process.env.MONGO_URI || '',
-    }
-}
+  PORT: process.env.PORT || 8080,
+  NODE_ENV: process.env.NODE_ENV || 'development',
+  DB: {
+    MONGO_URI: process.env.MONGO_URI || '',
+  },
+};
--- server/src/utils/catchAsync.ts
+++ server/src/utils/catchAsync.ts
@@ -2,6 +2,5 @@ import { Request, Response, NextFunction } from 'express';
 
 type AsyncFn = (req: Request, res: Response, next: NextFunction) => Promise<void>;
 
-export const catchAsync =
-  (fn: AsyncFn) => (req: Request, res: Response, next: NextFunction) =>
-    Promise.resolve(fn(req, res, next)).catch(next);
+export const catchAsync = (fn: AsyncFn) => (req: Request, res: Response, next: NextFunction) =>
+  Promise.resolve(fn(req, res, next)).catch(next);
