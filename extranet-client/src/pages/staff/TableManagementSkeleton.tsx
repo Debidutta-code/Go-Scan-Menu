bf5939b3b1b41f44a4cb9346dcd2351011836c27
Debidutta <debiduttaacharya2002@gmail.com>
2026-02-23 16:22:08 +0530

    add skeleton to table management page

---
+++ extranet-client/src/pages/staff/TableManagementSkeleton.tsx
@@ -0,0 +1,34 @@
+// src/pages/staff/TableManagementSkeleton.tsx
+import React from 'react';
+import './TableManagementSkeleton.css';
+
+export const TableManagementSkeleton: React.FC = () => {
+    return (
+        <div className="table-management-skeleton">
+            <div className="skeleton-toolbar">
+                <div className="skeleton-title skeleton-shimmer"></div>
+                <div className="skeleton-toolbar-actions">
+                    <div className="skeleton-button skeleton-shimmer"></div>
+                    <div className="skeleton-button skeleton-shimmer"></div>
+                    <div className="skeleton-button skeleton-shimmer"></div>
+                    <div className="skeleton-button skeleton-shimmer"></div>
+                </div>
+            </div>
+
+            <div className="skeleton-content">
+                <div className="skeleton-panel-header skeleton-shimmer"></div>
+
+                {[1, 2].map((group) => (
+                    <div key={group} className="skeleton-location-group">
+                        <div className="skeleton-location-header skeleton-shimmer"></div>
+                        <div className="skeleton-grid">
+                            {Array.from({ length: group === 1 ? 12 : 8 }).map((_, i) => (
+                                <div key={i} className="skeleton-cube skeleton-shimmer"></div>
+                            ))}
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
